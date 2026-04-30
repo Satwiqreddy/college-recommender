7f68713a592cbd3823840b103c4b10442b4a2497
Satwiq Reddy <satwiqreddy@gmail.com>
2026-04-30 12:04:39 +0530

    Fix server.js for Azure Named Pipes

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
-const cli = require('next/dist/cli/next-start');
-cli.nextStart(['-p', process.env.PORT || '8080']);
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+
+// Azure IISNode passes a named pipe path to process.env.PORT on Windows App Services
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('Internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on Azure IISNode port/pipe: ${port}`);
+    });
+});
